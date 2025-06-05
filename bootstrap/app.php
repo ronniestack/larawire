236924a5b6c239da6749dc4658d5a9a9d0bb7f9f
ronniestack <ronniecalleda08@gmail.com>
2025-06-05 20:14:56 +0800

    middlewares for 3 users

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleWare;
+use App\Http\Middleware\PhysicianMiddleWare;
+use App\Http\Middleware\PatientMiddleWare;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin' => AdminMiddleware::class,
+            'physician' => PhysicianMiddleWare::class,
+            'patient' => PatientMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
