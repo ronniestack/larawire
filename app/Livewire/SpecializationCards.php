77773d800290ea80834a4158ccada237c347d355
ronniestack <ronniecalleda08@gmail.com>
2025-06-05 21:12:57 +0800

    add component logics

---
+++ app/Livewire/SpecializationCards.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SpecializationCards extends Component
+{
+    public function render()
+    {
+        return view('livewire.specialization-cards');
+    }
+}
